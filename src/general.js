f1811108758f3b9f1e900c7ca85aa780bcf33de1
Varoon Kumar <varoonk21@gmail.com>
2025-07-04 07:48:05 +0500

    finished the generator

---
+++ src/general.js
@@ -0,0 +1,25 @@
+const generateBtn = document.getElementById('generate-btn');
+const generateContainer = document.getElementById('generate-container');
+const scanBtn = document.getElementById('scan-btn');
+const scannerContainer = document.getElementById('scanner-container');
+
+scanBtn.addEventListener('click', ()=>{
+    scannerContainer.classList.remove("hidden");
+    scannerContainer.classList.add('flex', 'flex-col', 'md:flex-row');
+    generateContainer.classList.add("hidden");
+    generateContainer.classList.remove('flex', 'flex-col', 'md:flex-row');
+    scanBtn.classList.add('bg-white');
+    scanBtn.classList.replace('text-white', 'text-black');
+    generateBtn.classList.remove('bg-white');
+    generateBtn.classList.replace('text-black', 'text-white');
+});
+generateBtn.addEventListener('click', ()=>{
+    generateContainer.classList.remove("hidden");
+    generateContainer.classList.add('flex', 'flex-col', 'md:flex-row');
+    scannerContainer.classList.add("hidden");
+    scannerContainer.classList.remove('flex', 'flex-col', 'md:flex-row');
+    generateBtn.classList.add('bg-white');
+    generateBtn.classList.replace('text-white', 'text-black');
+    scanBtn.classList.remove('bg-white');
+    scanBtn.classList.replace('text-black', 'text-white');
+});
